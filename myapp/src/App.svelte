9df85e0d1287c8ba13561f7310869e2827df5cde
Ethan Henrickosn <henricksonethan@gmail.com>
2023-02-14 18:43:32 -0600

    Added like button
    Added Like button, cleaned up code and made it ready to connect to database, and changed the style to something better.

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -7,19 +7,25 @@
     let user1 = {
         imageSrc: "https://petapixel.com/assets/uploads/2022/08/fdfs11-800x533.jpg",
         userName: "Ethan Henrickson",
-        description: "A nice photo that I captured last summer in the lost city of Atlantis. Its probably one of my favorite photos"
+        userId: 1,
+        description: "A nice photo that I captured last summer in the lost city of Atlantis. Its probably one of my favorite photos",
+        liked: false
     }
 
     let user2 = {
         imageSrc: "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1440,f_auto/50491001_kupidw.png",
         userName: "Zach Nickolas",
-        description: "I love rats. They are cool."
+        userId: 2,
+        description: "I love rats. They are cool.",
+        liked: true
     }
 
     let user3 = {
         imageSrc: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjZXxlbnwwfHwwfHw%3D&w=1000&q=80",
         userName: "Random Dude",
-        description: "I hate rats. They suck!"
+        userId: 3,
+        description: "I hate rats. They suck!",
+        liked: false
     }
     
 </script>
--- myapp/src/parts/normalCard.svelte
+++ myapp/src/parts/normalCard.svelte
@@ -1,29 +1,45 @@
+<!--
+    Must add database to add and delete likes from clients account
+-->
+
 <script>
     export let userName = "Anonymous";
     export let description = "";
-    export let imageSrc
+    export let imageSrc;
+    export let userId;
+    export let liked = false;
+
+    function addLike() {
+        liked = true;
+        //send data to database to like
+    }
+
+    function removeLike() {
+        liked = false;
+        //send data to database to remove like
+    }
 </script>
 
 <style>
-.card {
-    color: white;
-    width: 62%;
-    border: 0px white solid;
-    border-radius: 25px;
-    background-color: hsl(185, 42%, 40%);
-    margin-top: 30px ;
-    margin-left: 2%;
-    margin-right: 2%;
-    padding: 2%;
-    box-shadow: 0px 0px .7em rgb(179, 179, 179);
+
+* {
+    font-size: large;
 }
 
-.card:hover {
-    box-shadow: 0px 0px 1.4em rgb(179, 179, 179); 
-    transition: .2s;
+.card {
+    width: 62%;
+    color: white;
+    background-color: hsl(185, 24%, 48%);
+    border: 0px black solid;
+    border-radius: 25px;
+    margin: 2em 1em;
+    padding: 1.25em;
+    box-shadow: 0px 0px .7em rgb(149, 149, 149);
 }
-.card:not(:hover) {
-    transition: .2s;
+
+.card a {
+    color: white;
+    text-decoration: none;
 }
 
 .image {
@@ -31,19 +47,77 @@
     border-radius: 15px;
 }
 
+.nameHeader {
+    display: flex;
+}
+
 .nameElement{
-    padding-left: 2%;
+    padding-left: .75em;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+.nameElement:hover {
+    color: hsl(185, 35%, 20%);
+    transition: .3s;
+}
+.nameElement:not(:hover){
+    transition: .3s;
+}
+
+.growElement {
+    flex-grow: 1;
+}
+
+.likeButtonElement {
+    border: 0px solid;
+    background-color: transparent !important;
+    font-size: 2.5rem;
+    margin: 0px;
+    padding: 0px .5rem 0px 0px;
+    cursor: pointer;
+}
+
+.likeColor {
+    color: hsl(185, 35%, 20%);
+}
+.likeColor:active {
+    color: white;
+    transition: .3s;
+}
+
+.unlikeColor {
+    color: white;
+}
+.unlikeColor:active {
+    color: hsl(185, 35%, 20%);
+    transition: .3s;
 }
 
 .descriptionElement {
     font-style: italic;
+    margin: 0px;
 }
 
 </style>
 <main>
     <div class="card">
         <img src="{imageSrc}" alt="" class="image">
-        <h3 class="nameElement">{userName}</h3>
+        <div class="nameHeader">
+            <a href="www.localsocial.com/user/{userId}.html">
+                <h3 class="nameElement">{userName}</h3>
+            </a>
+
+            <div class="growElement"></div>
+            {#if liked}
+            <button type="button" class="likeButtonElement likeColor" on:click={removeLike}>
+                &#9825
+            </button>
+            {:else}
+            <button type="button" class="likeButtonElement unlikeColor" on:click={addLike}>
+                &#9825
+            </button>
+            {/if}
+        </div>
         <p class="descriptionElement">{description}</p>
     </div>
 </main>
